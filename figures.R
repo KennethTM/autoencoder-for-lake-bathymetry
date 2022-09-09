3fa13c77a8c6c2c8c3518f80fcce1bfe9f9e0acf
Kenneth Thorø Martinsen <kenneth2810@gmail.com>
2022-09-09 12:59:38 +0200

    Only show test set data in figure 4.

--- figures.R
+++ figures.R
@@ -221,44 +221,39 @@ ggsave("figures/figure_3.png", figure_3, width = 174, height = 100, units = "mm"
 #Histograms with performance metrics for best model and obs vs pred avg elevation (2x2 plot) for all lakes and test set only
 model_performance <- read_csv("data/lake_model_performance.csv")
 
-all_and_test <- bind_rows(
-  add_column(model_performance, Data = "All"),
-  add_column(model_performance[model_performance$partition == "test",], Data = "Test"))
+model_performance_test <- model_performance |> 
+  filter(partition == "test")
 
-fig_4_a <- all_and_test |> 
-  ggplot(aes(mae, fill=Data))+
-  geom_histogram(position = "identity", col="black")+
-  scale_fill_manual(values=c("All" ="grey", "Test" ="white"))+
+fig_4_a <- model_performance_test |> 
+  ggplot(aes(mae))+
+  geom_histogram(position = "identity", col="black", fill="white")+
   xlab("Mean absolute error (m)")+
   ylab("Count")+
   scale_y_continuous(expand = expansion(mult = c(0, 0.05)))+
-  scale_x_continuous(expand = expansion(mult = c(0.05, 0.05)), breaks = seq(0, 8, 2))
+  scale_x_continuous(expand = expansion(mult = c(0.05, 0.05)), breaks = seq(0, 10, 2))
 
-fig_4_b <- all_and_test |> 
-  ggplot(aes(rmse, fill=Data))+
-  geom_histogram(position = "identity", col="black")+
-  scale_fill_manual(values=c("All" ="grey", "Test" ="white"))+
+fig_4_b <- model_performance_test |> 
+  ggplot(aes(rmse))+
+  geom_histogram(position = "identity", col="black", fill="white")+
   xlab("Root mean squared error (m)")+
   ylab("Count")+
   scale_y_continuous(expand = expansion(mult = c(0, 0.05)))+
   scale_x_continuous(expand = expansion(mult = c(0.05, 0.05)), breaks = seq(0, 10, 2))
 
-fig_4_c <- all_and_test |> 
-  ggplot(aes(corr, fill=Data))+
-  geom_histogram(position = "identity", col="black")+
-  scale_fill_manual(values=c("All" ="grey", "Test" ="white"))+
+fig_4_c <- model_performance_test |> 
+  ggplot(aes(corr))+
+  geom_histogram(position = "identity", col="black", fill="white")+
   xlab("Pearson correlation coefficient")+
   ylab("Count")+
   scale_y_continuous(expand = expansion(mult = c(0, 0.05)))+
-  scale_x_continuous(expand = expansion(mult = c(0.05, 0.05)), breaks = seq(-0.4, 1, 0.2))
+  scale_x_continuous(expand = expansion(mult = c(0.05, 0.05)), breaks = seq(-0.2, 1, 0.2))
 
-fig_4_d <- all_and_test |> 
-  ggplot(aes(obs_mean, pred_mean, fill=Data))+
+fig_4_d <- model_performance_test |> 
+  ggplot(aes(obs_mean, pred_mean))+
   geom_abline(intercept = 0, slope = 1, linetype=3)+
-  geom_point(shape=21)+
+  geom_point(shape=21, fill="white")+
   ylim(-6, 80)+
   xlim(-6, 80)+
-  scale_fill_manual(values=c("All" ="grey", "Test" ="white"))+
   xlab("Observed mean elevation (m)")+
   ylab("Predicted mean elevation (m)")
 
